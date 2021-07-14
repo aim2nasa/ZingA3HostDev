2b69b6daa7ccc6dd7d005a96313c9daf09d75eb4
Rossi Kwak <rossi.kwak@gmail.com>
2021-07-14 14:18:03 +0900

    Cypress External Response:20
    1. In CyFxRecvBuffer, when you perform CyU3PDmaChannelWaitForCompletion, pass CYU3P_WAIT_FOREVER as timeout option.
    
    https://cypress.force.com/customer/Comm_CaseDetail?id=5001P00001BnSQi

--- uhbuf.c
+++ uhbuf.c
@@ -114,7 +114,7 @@ CyFxRecvBuffer (
     	return status;
     }
 
-    status = CyU3PDmaChannelWaitForCompletion (inpCh, CYU3P_NO_WAIT);
+    status = CyU3PDmaChannelWaitForCompletion (inpCh, CYU3P_WAIT_FOREVER);
     if(status!=CY_U3P_SUCCESS) {
     	CyU3PDebugPrint (4,"[CyFxRecvBuffer] CyU3PDmaChannelWaitForCompletion error=0x%x\r\n",status);
     	return status;
